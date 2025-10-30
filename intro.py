edef3cec51ed685301f8f4233e34e44bf9cc23ea
abrahamswebstudio <abrahamwochieng@gmail.com>
2025-10-31 00:04:36 +0300

    First Commit

---
+++ intro.py
@@ -0,0 +1,12 @@
+#We Just Getting STVRT3D!
+import mysql.connector
+
+mydb = mysql.connector.connect(host="localhost", user="root", passwd="30/Oct.db!MySQL.2025")
+
+mycursor = mydb.cursor()
+
+mycursor.execute("Select * from genixdb.stvrt3d")
+
+
+for i in mycursor:
+    print(i)
